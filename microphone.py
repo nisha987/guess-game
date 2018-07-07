03587d8ac942df6834f25f96051bec7616e51a61
nisha987 <30970290+nisha987@users.noreply.github.com>
2018-07-07 21:25:15 +0530

    Add files via upload

---
+++ microphone.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+r=sr.Recognizer()
+
+mic=sr.Microphone()
+
+sr.Microphone.list_microphone_names()
+
+mic=sr.Microphone(device_index=0)
+with mic as source:
+    audio=r.listen(source)
+    
+r.recognize_google(audio)
+
+##with mic as source:
+##    r.adjust_for_ambient_noise(source)
+##    audio.r.listen(source)
+    
+##r.recognize_google(audio)
+
